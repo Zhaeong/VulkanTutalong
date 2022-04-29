d7573dc8dc26c911d9650bc3c22c0cadccc7f33b
Zhaeong <owen_zzz@hotmail.com>
2022-04-29 00:24:27 -0400

    removed unused static func

--- include/vk_model.hpp
+++ include/vk_model.hpp
@@ -43,17 +43,6 @@ struct UniformBufferObject {
   alignas(16) glm::mat4 model;
   alignas(16) glm::mat4 view;
   alignas(16) glm::mat4 proj;
-
-  static VkDescriptorSetLayoutBinding getUBOLayoutBinding() {
-
-    VkDescriptorSetLayoutBinding uboLayoutBinding{};
-    uboLayoutBinding.binding = 0;
-    uboLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-    uboLayoutBinding.descriptorCount = 1;
-    uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
-    uboLayoutBinding.pImmutableSamplers = nullptr;
-    return uboLayoutBinding;
-  }
 };
 
 class VkModel {
